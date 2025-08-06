169069cdd80241832d8284fae6920cf95e1ff8ef
Yarre <yaroslav.syvukha@gmail.com>
2025-08-06 16:12:17 +0200

    feat: base layout

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,11 @@
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'off',
+    },
+  },
+];
---
+++ libs/application/README.md
@@ -0,0 +1,7 @@
+# application
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build application` to build the library.
